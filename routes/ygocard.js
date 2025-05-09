cc082e354081620acaeacff16c41235748cab6eb
Daniel Sauceda <dsauce15@calstatela.edu>
2025-05-08 20:14:44 -0700

    Implemented the second part of routes/ygocard.js

--- routes/ygocard.js
+++ routes/ygocard.js
@@ -43,4 +43,41 @@ router.get('/', async (req, res) => {
     }
 });
 
+/**
+ * Returns yugioh card details by its id
+ *
+ * @api {GET} /ygocard/:id
+ * @apiParam {string} id Card unique ID
+ *
+ * @apiExample localhost:8888/ygocard/1234
+ */
+
+router.get('/:id', async(req, res) => {
+    try{
+        const{id} = req.params;
+
+        if(!id) {
+            return res.status(400).json({
+                error: 'Missing the required path parameter: id'
+            });
+        }
+        
+        const cardData = await api.id(id);
+
+        if(!cardData || cardData.length === 0) {
+            return res.status(404).json({
+                error: 'Card not found'
+            });
+        }
+        
+        await db.insert('SearchHistorySelection', {id});
+        res.json(cardData);
+    }
+    catch(err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
 export default router;
